1dc28ac94f6399051f68ef7f6f30fab670ba2fd7
Binay bk <khadkabinay@yahoo.com>
2021-03-23 18:34:00 -0500

    Adds Header and Footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
-    <div>
+    <>
+    <Header />
+    <main>
       <h1>Welcome to E-Pasal</h1>
-    </div>
+    </main>
+    <Footer />
+    </>
   )
 }
 
