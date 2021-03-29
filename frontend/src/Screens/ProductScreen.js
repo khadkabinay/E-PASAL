65e9256b58b286a2d8da3de892849f322701f29a
Binay bk <khadkabinay@yahoo.com>
2021-03-28 23:19:12 -0500

    Updates productScreen component

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import products from '../products'
+import Rating from '../Components/Rating'
+import { Card, Image, Col, Row, Button, ListGroup, ListGroupItem  } from 'react-bootstrap'
 
-const ProductScreen = () => {
+const ProductScreen = ({ match }) => {
+    const product = products.find(p => p._id === match.params.id)
     return (
         <div>
-            Product
+            <h1>product</h1>
         </div>
     )
 }
