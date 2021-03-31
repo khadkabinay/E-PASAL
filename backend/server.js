24b4ca62017baa95fa69b37d5b59d28ce2a59026
Binay bk <khadkabinay@yahoo.com>
2021-03-31 11:28:25 -0500

    Adds all products and single product route

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const  products = require('./data/products')
 const app = express()
 const PORT = 5000
 
 app.get('/', (req, res) =>{
     res.send('API is running ...')
+})
+
+app.get('/API/product', (req, res) =>{
+    res.json(products)
+})
+
+app.get('/API/product/:id', (req, res) =>{
+    const product = products.find(p => p._id === req.params.id)
+    res.json(product)
 })
 
 
