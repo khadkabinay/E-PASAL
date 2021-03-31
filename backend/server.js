0b2f97ce53b77b53f865ffc709a4bbd03f3518d0
Binay bk <khadkabinay@yahoo.com>
2021-03-30 22:35:52 -0500

    Tests route

--- backend/server.js
+++ backend/server.js
@@ -2,5 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 
+app.get('/', (req, res) =>{
+    res.send('API is running ...')
+})
+
 
 app.listen(PORT , console.log(`Server running on port ${PORT}`))
