f2f684f534912eecd60a450eaa47ba7d8defaba2
Binay bk <khadkabinay@yahoo.com>
2021-04-02 12:43:18 -0500

    Adds sample data for users

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs'
+const users = [
+    {
+        name: 'Binu Bk',
+        email: 'adminbin@example.com',
+        password: bcrypt.hashSync('234567', 10),
+        isAdmin: true
+    },
+    {
+        name: 'Raju P',
+        email: 'raju@example.com',
+        password: bcrypt.hashSync('234567', 10),
+    },
+    {
+        name: 'sonu G',
+        email: 'sonu@example.com',
+        password: bcrypt.hashSync('234567', 10),
+    },
+
+]
+
+module.exports = users
