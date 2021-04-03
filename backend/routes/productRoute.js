94de08d67ee58ad138f10dd6784426b4e051bcc1
Binay bk <khadkabinay@yahoo.com>
2021-04-02 22:50:27 -0500

    Adds asyncHandler middleware

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,27 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const asyncHandler = require('express-async-handler')
 const Product = require('../models/productModel')
 
 
-router.get('/', async (req, res) =>{
-    try {
+//@desc Fetch all products
+router.get('/', asyncHandler (async (req, res) => {
         const products = await Product.find({})
         res.json(products)
-        
-    } catch (error) {
-        console.log(error)
-    }
-})
+}))
 
-router.get('/:id', async (req, res) =>{
 
-    try {
-        const product = products.find(p => p._id === req.params.id)
-        res.json(product)
-        
-    } catch (error) {
-        console.log(error)
-    }
-})
+//@desc Fetch single product
+router.get('/:id', asyncHandler(async (req, res) =>{
+        const product = await Product.findById(req.params.id)
+        if(product){
+            res.json(product)
+        }else{
+            res.status(404).json({message:'Product not found'})
+        }
+}))
+
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const dotenv = require('dotenv')
-const products = require('./data/products')
-const connectDB = require('./config/db')
 const app = express()
+const dotenv = require('dotenv')
+const productRoutes = require('./routes/productRoute')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
 
 dotenv.config()
@@ -11,6 +11,7 @@ app.get('/', (req, res) =>{
     res.send('API is running ...')
 })
 
+app.use('/api/products', productRoutes)
 
 
 app.listen(PORT , console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
