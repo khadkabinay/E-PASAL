02ab0df545e9569d1aed1b332a881212eb116c9e
Binay bk <khadkabinay@yahoo.com>
2021-04-08 18:45:36 -0500

    Updates productRoute

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -17,7 +17,7 @@ router.get('/:id', asyncHandler(async (req, res) =>{
         if(product){
             res.json(product)
         }else{
-            res.status(404).json({message:'Product not found'})
+            res.status(404).json({'message':'Product not found'})
             
         }
 }))
