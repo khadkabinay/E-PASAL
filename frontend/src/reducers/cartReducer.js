44a97751b836d2a7c526d48278cf75adcb0d8ca9
Binay bk <khadkabinay@yahoo.com>
2021-04-15 15:14:54 -0500

    Adds cartActions

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -1,6 +1,6 @@
 import { CART_ADD_ITEM } from '../constants/cartConstants'
 
-export const cartReducer =(state ={cartItem: []}, action) => {
+export const cartReducer =(state ={cartItems: []}, action) => {
     switch(action.type) {
         case CART_ADD_ITEM:
             const item = action.payload 
@@ -17,5 +17,7 @@ export const cartReducer =(state ={cartItem: []}, action) => {
                     ...state, cartItems: [...state.cartItems, item]
                 }
             }
+        default:
+            return state
     }
 }
