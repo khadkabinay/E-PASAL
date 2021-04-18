c1297b6f526fb41201c0839bdced98bbbca77a0d
Binay bk <khadkabinay@yahoo.com>
2021-04-18 15:05:46 -0500

    Fixes axios call url in cartActions

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -4,7 +4,7 @@ import { CART_ADD_ITEM }  from '../constants/cartConstants'
 
 export const  addToCart = (id, qty) => async (dispatch, getState) => {
 
-    const { data } = await axios.get(`/api/product/${id}`)
+    const { data } = await axios.get(`/api/products/${id}`)
 
     dispatch({
         type : CART_ADD_ITEM,
