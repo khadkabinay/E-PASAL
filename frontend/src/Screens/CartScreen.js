4a720fc5df73fbcc239357ac260c42631f8e6e8b
Binay bk <khadkabinay@yahoo.com>
2021-04-20 22:58:36 -0500

    Adds details  in cartScreen

--- frontend/src/Screens/CartScreen.js
+++ frontend/src/Screens/CartScreen.js
@@ -3,6 +3,7 @@ import { useDispatch , useSelector } from 'react-redux'
 import { addToCart } from '../actions/cartActions'
 import { Link } from 'react-router-dom'
 import Message  from '../Components/Message'
+import { Row, Col , Image } from 'react-bootstrap'
 
 const CartScreen = ({ match, location, history }) => {
     const productId = match.params.id
@@ -25,7 +26,15 @@ const CartScreen = ({ match, location, history }) => {
         <>
         <h1>Shopping Cart</h1>
         {cartItems.length === 0 ? (<Message> Your cart is empty</Message>): 
-            null
+        (cartItems.map(item => (
+            <Row>
+                <Image src={item.image} />
+                <Col md={2}>{item.name}</Col>
+                <Col md={2}>{item.price}</Col>
+            </Row>
+
+        )  
+        ))
         }
         </>
     )
