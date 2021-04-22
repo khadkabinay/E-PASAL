b3707c9c3c9ae4e302821d6a1cff9dbf2d999d2e
Binay bk <khadkabinay@yahoo.com>
2021-04-21 23:42:08 -0500

    Adds select options for quantity

--- frontend/src/Screens/CartScreen.js
+++ frontend/src/Screens/CartScreen.js
@@ -3,7 +3,7 @@ import { useDispatch , useSelector } from 'react-redux'
 import { addToCart } from '../actions/cartActions'
 import { Link } from 'react-router-dom'
 import Message  from '../Components/Message'
-import { Row, Col , Image } from 'react-bootstrap'
+import { Row, Col , Image, Form } from 'react-bootstrap'
 
 const CartScreen = ({ match, location, history }) => {
     const productId = match.params.id
@@ -27,10 +27,17 @@ const CartScreen = ({ match, location, history }) => {
         <h1>Shopping Cart</h1>
         {cartItems.length === 0 ? (<Message> Your cart is empty</Message>): 
         (cartItems.map(item => (
-            <Row>
+            <Row key={item.product}>
                 <Image src={item.image} />
                 <Col md={2}>{item.name}</Col>
                 <Col md={2}>{item.price}</Col>
+                <Col>
+                <Form.Control as='select' value={item.qty} onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}>
+                        {[...Array(item.countInStock).keys()].map(x => (
+                            <option key={x + 1} value={x + 1}>{x + 1}</option>
+                        ))}
+                </Form.Control>
+                </Col>
             </Row>
 
         )  
