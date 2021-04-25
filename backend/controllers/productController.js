c6bd8a6e78753904bcefac36811e80ca999e59e3
Binay bk <khadkabinay@yahoo.com>
2021-04-25 00:05:22 -0500

    Adds getProducts and getProductById functions in controllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,1 +1,26 @@
+const asyncHandler = require('express-async-handler')
+const Product = require('../models/productModel')
+
+
+//@desc Fetch all products
+const getProducts = asyncHandler (async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+})
+
+
+
+//@desc Fetch single product
+const getProductById = asyncHandler(async (req, res) =>{
+    const product = await Product.findById(req.params.id)
+    if(product){
+        res.json(product)
+    }else{
+        res.status(404).json({'message':'Product not found'})
+        
+    }
+})
+
+
+module.exports = { getProducts, getProductById } 
 
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,25 +2,11 @@ const express = require('express')
 const router = express.Router()
 const asyncHandler = require('express-async-handler')
 const Product = require('../models/productModel')
+const {getProducts, getProductById } = require('../controllers/productController')
 
 
-//@desc Fetch all products
-router.get('/', asyncHandler (async (req, res) => {
-        const products = await Product.find({})
-        res.json(products)
-}))
-
-
-//@desc Fetch single product
-router.get('/:id', asyncHandler(async (req, res) =>{
-        const product = await Product.findById(req.params.id)
-        if(product){
-            res.json(product)
-        }else{
-            res.status(404).json({'message':'Product not found'})
-            
-        }
-}))
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
 
 module.exports = router
