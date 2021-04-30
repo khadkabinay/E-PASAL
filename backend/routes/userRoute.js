fc8129285b2da434753ec7a43aa84baa6d7d42f1
Binay bk <khadkabinay@yahoo.com>
2021-04-30 14:38:41 -0500

    Adds protectRoute in userProfile

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const asyncHandler = require("express-async-handler");
 const Product = require("../models/productModel");
 const { authUser, getUserProfile } = require("../controllers/userController");
+const protectRoute = require("../middleware/authMiddleware");
 
 router.post("/login", authUser);
-router.get("/profile", getUserProfile);
+router.get("/profile", protectRoute, getUserProfile);
 
 module.exports = router;
