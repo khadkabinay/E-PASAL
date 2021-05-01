4618ddb088248b8b10ce50532e47d61abf0ccb94
Binay bk <khadkabinay@yahoo.com>
2021-05-01 00:44:13 -0500

    Updates authMiddleware and imports it in userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,7 +23,18 @@ const authUser = asyncHandler(async (req, res) => {
 
 //@desc User profile
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("success");
+  user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 module.exports = {
