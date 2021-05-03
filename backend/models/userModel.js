3dcec4318c07dcc19b0aee25f550bd42d6ec279c
Binay bk <khadkabinay@yahoo.com>
2021-05-03 00:16:29 -0500

    Updates userModel for hashing password before saving it

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,34 +1,46 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema = mongoose.Schema({
-    name:  {
-        type: String,
-        required: true
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email:  {
-        type: String,
-        required: true,
-        unique: true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:  {
-        type: String,
-        required: true
+    password: {
+      type: String,
+      required: true,
     },
-    isAdmin:  {
-        type: Boolean,
-        required: true,
-        default: false
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
     },
-}, {
-    timestamps: true
-})
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // instantiate a method to compare old and new password
-userSchema.methods.matchPassword = async function(enteredPassword){
-    return await bcrypt.compare(enteredPassword, this.password)
-}
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-const User = mongoose.model('User', userSchema) 
+//saves a password after hashing
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
-module.exports = User
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
