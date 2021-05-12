3df66a7b6ca897dc580951789549107c5c35279e
Binay bk <khadkabinay@yahoo.com>
2021-05-11 19:07:48 -0500

    Adds dropdown nav

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 const Header = () => {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logOutHandler = () => {
+    console.log("Logout");
+  };
   return (
     <header>
       <Navbar bg="light" expand="lg">
@@ -14,7 +22,18 @@ const Header = () => {
               <Link className="mr-5" to="/cart">
                 Cart
               </Link>
-              <Link to="/login">Sign In</Link>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="nav-dropdown">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logOutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <Link to="/login">Sign In</Link>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
