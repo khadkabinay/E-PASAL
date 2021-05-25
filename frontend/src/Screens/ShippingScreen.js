8987192edfd8c93702b7a4a484589303f1511b25
Binay bk <khadkabinay@yahoo.com>
2021-05-25 17:56:32 -0500

    Updates ShippingScreen

--- frontend/src/Screens/ShippingScreen.js
+++ frontend/src/Screens/ShippingScreen.js
@@ -1,5 +1,7 @@
-import React from "react";
-import { Col, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import FormContainer from "../Components/FormContainer";
 
 const ShippingScreen = () => {
   return <div>Shipping Screen</div>;
