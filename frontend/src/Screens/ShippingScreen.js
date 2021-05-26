a2f51721656c499814702b37a840a2f084b960cf
Binay bk <khadkabinay@yahoo.com>
2021-05-26 16:25:20 -0500

    Adds state on ShippingScreen

--- frontend/src/Screens/ShippingScreen.js
+++ frontend/src/Screens/ShippingScreen.js
@@ -3,8 +3,13 @@ import { Form, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../Components/FormContainer";
 
-const ShippingScreen = () => {
-  return <div>Shipping Screen</div>;
+const ShippingScreen = ({ history }) => {
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [postalCode, setpostalCode] = useState("");
+  const [country, setCountry] = useState("");
+
+  return <FormContainer>Shipping Screen</FormContainer>;
 };
 
 export default ShippingScreen;
