633948e492b5f648cfdcb7c809f77f7bc48a7f57
Binay bk <khadkabinay@yahoo.com>
2021-06-02 00:05:24 -0500

    Adds no changes

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,34 +1,37 @@
-import React, { useEffect } from 'react'
-import { Row, Col } from 'react-bootstrap'
-import Product from '../Components/Product'
-import { useDispatch, useSelector } from 'react-redux'
-import { listProducts } from '../actions/productAction'
-import Loader from '../Components/Loader'
-import Message from '../Components/Message'
-
-
+import React, { useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../Components/Product";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productAction";
+import Loader from "../Components/Loader";
+import Message from "../Components/Message";
 
 const HomeScreen = () => {
-        const dispatch = useDispatch()
-        const productList = useSelector(state => state.productList)
-        const { loading, error, products } = productList
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
-    useEffect(() => {
-        dispatch(listProducts())
-    }, [dispatch])
-    return (
-        <>
-        <h2>Latest Products</h2>
-        { loading ? <Loader /> : error ? <Message >{error}</Message> :
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+  return (
+    <>
+      <h3>Latest Products</h3>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message>{error}</Message>
+      ) : (
         <Row>
-        {products.map(product =>(
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                    <Product product={product} />
-                </Col>
-        ))}
-        </Row>}
-        </>
-    )
-}
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
