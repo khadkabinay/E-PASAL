eab908bdb5e811b977246041cb8b0d0dcd31515a
Binay bk <khadkabinay@yahoo.com>
2021-06-03 13:03:26 -0500

    Updates PaymentScreen component

--- frontend/src/Screens/PaymentScreen.js
+++ frontend/src/Screens/PaymentScreen.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import FormContainer from "../Components/FormContainer";
+import CheckoutSteps from "../Components/CheckoutSteps";
+import { savePaymentMethod } from "../actions/cartActions";
 
-const PaymentScreen = () => {
-  return <div></div>;
+const PaymentScreen = ({ history }) => {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  if (!shippingAddress) {
+    history.push("/shipping");
+  }
+  const [paymentMethod, setPaymentMethod] = useState("PayPal");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod({ paymentMethod }));
+    history.push(`/placeorder`);
+  };
+
+  return (
+    <FormContainer>
+      <h1>Payment Method</h1>
+      <CheckoutSteps step1 step2 step3 />
+      <Form onSubmit={submitHandler}>
+        <Button type="submit" variant="primary">
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
 };
 
 export default PaymentScreen;
