39996508b8bad3bf637851ca524f17b9b99f52fc
Binay bk <khadkabinay@yahoo.com>
2021-06-06 15:05:59 -0500

    Updates PlaceOrderScreen

--- frontend/src/Screens/PlaceOrderScreen.js
+++ frontend/src/Screens/PlaceOrderScreen.js
@@ -4,9 +4,21 @@ import { useDispatch, useSelector } from "react-redux";
 import CheckoutSteps from "../Components/CheckoutSteps";
 
 const PlaceOrderScreen = () => {
+  const cart = useSelector((state) => state.cart);
+  console.log(cart);
   return (
     <>
       <CheckoutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              {cart.shippingAddress.address}, {cart.shippingAddress.city},
+              {cart.shippingAddress.postalCode}, {cart.shippingAddress.country}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
     </>
   );
 };
