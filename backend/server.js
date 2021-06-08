0bc17c8e03e78567df0dd5751d00a7a51607ad02
Binay bk <khadkabinay@yahoo.com>
2021-06-07 21:18:37 -0500

    Adds orderRoutes in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,25 @@
-const express = require('express')
-const app = express()
-const dotenv = require('dotenv')
-const productRoutes = require('./routes/productRoute')
-const userRoutes = require('./routes/userRoute')
-const connectDB = require('./config/db')
-const notFound = require('./middleware/errorHandler')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/usersRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const connectDB = require("./config/db");
+const notFound = require("./middleware/errorHandler");
+const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+dotenv.config();
+connectDB();
+app.get("/", (req, res) => {
+  res.send("API is running ...");
+});
 
-app.use(express.json())
-dotenv.config()
-connectDB()
-app.get('/', (req, res) =>{
-    res.send('API is running ...')
-})
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
-
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
-
-
-
-app.listen(PORT , console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
