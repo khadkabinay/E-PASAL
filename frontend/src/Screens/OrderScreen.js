7b6b68efb5e83efd499372d3117db77894c7e87b
Binay bk <khadkabinay@yahoo.com>
2021-06-12 11:19:53 -0500

    Adds OrderScreen

---
+++ frontend/src/Screens/OrderScreen.js
@@ -0,0 +1,111 @@
+import React, { useEffect } from "react";
+import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../Components/Loader";
+import { getOrderDetails } from "../actions/orderActions";
+
+const OrderScreen = ({ match }) => {
+  const orderId = match.params.id;
+  const dispatch = useDispatch();
+  const orderDetails = useSelector((state) => state.orderDetails);
+  const { order, loading, error } = orderDetails;
+
+  useEffect(() => {
+    dispatch(getOrderDetails(orderId));
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant="danger">{error}</Message>
+  ) : (
+    <>
+      <h1>Oder {order._id}</h1>
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>Shipping</h3>
+              {order.shippingAddress.address}, {order.shippingAddress.city},
+              {order.shippingAddress.postalCode},{" "}
+              {order.shippingAddress.country}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h3>Payment Method</h3>
+              <strong>Method: </strong>
+              {order.paymentMethod}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h3>Order Items</h3>
+              {order.orderItems.length === 0 ? (
+                <h2>Your order is Empty</h2>
+              ) : (
+                <ListGroup variant="flush">
+                  {order.orderItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                        <Col>
+                          <Link to={`/product/${item.productId}`}>
+                            {item.name}
+                          </Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} X ${item.price} = ${item.qty * item.price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items</Col>
+                  <Col>${order.itemsPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping</Col>
+                  <Col>${order.shippingPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax</Col>
+                  <Col>${order.taxPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>${order.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default OrderScreen;
