c0abcff3c75d4609edd3655fc8030f7de81f5a63
Binay bk <khadkabinay@yahoo.com>
2021-06-20 12:07:35 -0500

    Changes server.js

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 
 app.get("/api/config/paypal", (req, res) => {
+  console.log(process.env.PAYPAL_CLIENT_ID);
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
 
