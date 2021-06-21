0c8251cb2821355e15a71216f7fde1ce80c4bb56
Binay bk <khadkabinay@yahoo.com>
2021-06-21 16:22:59 -0500

    Adds ORDER_MY_LIST constants and reducer

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -9,6 +9,9 @@ import {
   ORDER_PAY_SUCCESS,
   ORDER_PAY_FAIL,
   ORDER_PAY_RESET,
+  ORDER_MY_LIST_REQUEST,
+  ORDER_MY_LIST_SUCCESS,
+  ORDER_MY_LIST_FAIL,
 } from "../constants/orderConstants";
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -50,6 +53,18 @@ export const orderPayReducer = (state = {}, action) => {
       return { loading: false, error: action.payload };
     case ORDER_PAY_RESET:
       return {};
+    default:
+      return state;
+  }
+};
+
+export const orderMyListReducer = (state = { orders: [] }, action) => {
+  switch (action.type) {
+    case ORDER_MY_LIST_REQUEST:
+      return { loading: true };
+    case ORDER_MY_LIST_SUCCESS:
+      return { loading: false, orders: action.payload };
+    case ORDER_MY_LIST_FAIL:
     default:
       return state;
   }
