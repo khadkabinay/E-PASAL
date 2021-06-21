aae6ceeb8aa5be489dcd446dae6980b08c27428f
Binay bk <khadkabinay@yahoo.com>
2021-06-21 16:07:08 -0500

    Adds /myorders route for getMyOrders

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,10 +4,12 @@ const {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  getMyOrders,
 } = require("../controllers/orderController");
 const { protectRoute } = require("../middleware/authMiddleware");
 
 router.route("/").post(protectRoute, addOrderItems);
+router.route("/myorders").get(protectRoute, getMyOrders);
 router.route("/:id").get(protectRoute, getOrderById);
 router.route("/:id/pay").put(protectRoute, updateOrderToPaid);
 
