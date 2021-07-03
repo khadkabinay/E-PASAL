fc174d87af4d9f3834ec43545dcc9f3ccf299d08
Binay bk <khadkabinay@yahoo.com>
2021-07-03 11:09:32 -0500

    Creates admin middlewares

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -27,6 +27,16 @@ const protectRoute = asyncHandler(async (req, res, next) => {
   }
 });
 
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an admin");
+  }
+};
+
 module.exports = {
   protectRoute,
+  admin,
 };
--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -7,10 +7,11 @@ const {
   getUserProfile,
   registerUser,
   updateUserProfile,
+  getUsers,
 } = require("../controllers/userController");
 const { protectRoute } = require("../middleware/authMiddleware");
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protectRoute, getUsers);
 router.post("/login", authUser);
 router
   .route("/profile")
