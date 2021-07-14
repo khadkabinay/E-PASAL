38812fb32001eb858a5ce77cb743026d6d112deb
Binay bk <khadkabinay@yahoo.com>
2021-07-13 22:04:38 -0500

    Updates Footer component

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React from "react";
+import { Container } from "react-bootstrap";
 
 const Footer = () => {
-    return (
-        <footer>
-            <Container className='text-center'>
-                <h5>Copyright &copy; E-Pasal</h5>
-            </Container>
-        </footer>
-    )
-}
+  return (
+    <footer>
+      <Container className="text-center">
+        <h4>Copyright &copy; E-Pasal</h4>
+      </Container>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
