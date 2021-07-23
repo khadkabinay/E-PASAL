12e6479262c9ebcfd390947bf23853ce338c974b
Binay bk <khadkabinay@yahoo.com>
2021-07-23 18:23:58 -0500

    Updates userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -90,7 +90,8 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-//@desc Get all users
+
+// @desc Get all users
 const getUsers = asyncHandler(async (req, res) => {
   const users = await User.find({});
   res.json(users);
