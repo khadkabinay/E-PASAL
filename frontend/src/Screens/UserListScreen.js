4387aaec525804ce70dbf30c153d4aca9edc7f41
Binay bk <khadkabinay@yahoo.com>
2021-08-06 12:45:56 -0500

    Adds admin menu on the nav dropdown menu

--- frontend/src/Screens/UserListScreen.js
+++ frontend/src/Screens/UserListScreen.js
@@ -9,6 +9,7 @@ import Message from "../Components/Message";
 const UserListScreen = () => {
   const dispatch = useDispatch();
   const userList = useSelector((state) => state.userList);
+  console.log(userList);
   const { loading, error, users } = userList;
 
   useEffect(() => {
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -73,7 +73,7 @@ export const updateProfileReducer = (state = {}, action) => {
       return state;
   }
 };
-export const userListReducer = (state = { user: [] }, action) => {
+export const userListReducer = (state = { users: [] }, action) => {
   switch (action.type) {
     case USER_LIST_REQUEST:
       return { loading: true };
