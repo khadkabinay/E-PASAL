d6bef9faee297fc7aa982b85b968ae5349fb15f7
Binay bk <khadkabinay@yahoo.com>
2022-03-29 14:17:54 -0500

    Adds createProduct and updateProduct controllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -17,7 +17,7 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-//@desc delete a product
+//@desc Delete a product
 const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
   if (product) {
@@ -25,6 +25,46 @@ const deleteProduct = asyncHandler(async (req, res) => {
     res.json({ message: "Product remove()" });
   } else {
     res.status(404).json({ message: "Product not found" });
+  }
+});
+
+//@desc Create a product
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/image/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+//@desc Update a product
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, brand, image, category, countInStock } =
+    req.body;
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.status(201).json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
   }
 });
 
--- frontend/src/Screens/ProductListScreen.js
+++ frontend/src/Screens/ProductListScreen.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { LinkContainer } from "react-router-bootstrap";
 import { Table, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { listProducts } from "../actions/productAction";
+import { listProducts, deleteProduct } from "../actions/productAction";
 import Loader from "../Components/Loader";
 import Message from "../Components/Message";
 
@@ -11,6 +11,13 @@ const ProductListScreen = ({ history, match }) => {
 
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
+
+  const productDelete = useSelector((state) => state.productDelete);
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = productDelete;
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
@@ -21,11 +28,11 @@ const ProductListScreen = ({ history, match }) => {
     } else {
       history.push("/login");
     }
-  }, [dispatch, history, userInfo]);
+  }, [dispatch, history, userInfo, successDelete]);
 
   const deleteHandler = (id) => {
     if (window.confirm("Are you sure ?")) {
-      // delete products
+      dispatch(deleteProduct(id));
     }
   };
 
@@ -45,6 +52,8 @@ const ProductListScreen = ({ history, match }) => {
           </Button>
         </Col>
       </Row>
+      {loadingDelete && <Loader />}
+      {errorDelete && <Message variant="danger">{errorDelete}</Message>}
       {loading ? (
         <Loader />
       ) : error ? (
