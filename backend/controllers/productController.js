bdf18bd6aea6e284b73ba3b5cce6dbf1c49e23e2
Binay bk <khadkabinay@yahoo.com>
2022-03-29 12:43:38 -0500

    Adds deleteProduct controller and route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -17,4 +17,15 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { getProducts, getProductById };
+//@desc delete a product
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product remove()" });
+  } else {
+    res.status(404).json({ message: "Product not found" });
+  }
+});
+
+module.exports = { getProducts, getProductById, deleteProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,18 @@
-const express = require('express')
-const router = express.Router()
-const asyncHandler = require('express-async-handler')
-const Product = require('../models/productModel')
-const {getProducts, getProductById } = require('../controllers/productController')
+const express = require("express");
+const router = express.Router();
+const asyncHandler = require("express-async-handler");
+const Product = require("../models/productModel");
+const {
+  getProducts,
+  getProductById,
+  deleteProduct,
+} = require("../controllers/productController");
+const { protectRoute, admin } = require("../middleware/authMiddleware");
 
+router.route("/").get(getProducts);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protectRoute, admin, deleteProduct);
 
-router.route('/').get(getProducts)
-router.route('/:id').get(getProductById)
-
-
-module.exports = router
+module.exports = router;
