fde0eaa3611de93d85634a9b750d8298dc48e817
Binay bk <khadkabinay@yahoo.com>
2022-03-30 10:03:37 -0500

    Adds image uploadRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/usersRoutes");
 const orderRoutes = require("./routes/orderRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
 const connectDB = require("./config/db");
 const notFound = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 5000;
@@ -18,10 +20,12 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/uploads", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.listen(
   PORT,
