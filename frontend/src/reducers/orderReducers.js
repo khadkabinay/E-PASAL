0a252a8257a2f7408405dd0443140b0685cfea11
Binay bk <khadkabinay@yahoo.com>
2022-03-30 12:36:34 -0500

    Adds orderListScreen

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -16,7 +16,6 @@ import {
   ORDER_LIST_FAIL,
   ORDER_LIST_REQUEST,
   ORDER_LIST_SUCCESS,
-  ORDER_LIST_RESET,
 } from "../constants/orderConstants";
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -86,8 +85,6 @@ export const orderListReducer = (state = { orders: [] }, action) => {
       return { loading: false, orders: action.payload };
     case ORDER_LIST_FAIL:
       return { loading: false, error: action.payload };
-    case ORDER_LIST_RESET:
-      return { orders: [] };
     default:
       return state;
   }
