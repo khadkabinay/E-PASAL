a8aafa79727b4e48eee77a9e5da14d1bd6533a49
Binay bk <khadkabinay@yahoo.com>
2022-06-13 15:42:30 -0500

    Adds submitHandler

--- frontend/src/Components/SearchBox.js
+++ frontend/src/Components/SearchBox.js
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
-const SearchBox = () => {
+const SearchBox = (history) => {
   const [keyword, setKeyword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (keyword.trim()) {
+      history.push(`/search/${keyword}`);
+    } else {
+      history.push("");
+    }
+  };
+
   return (
     <Form onSubmit={submitHandler} inline>
       <Form.Control
