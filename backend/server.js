b5320d2b9717ace39018811c502b37081366889b
Binay bk <khadkabinay@yahoo.com>
2022-10-15 12:07:24 -0500

    Adds Procfile and build script

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,6 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 dotenv.config();
 connectDB();
-app.get("/", (req, res) => {
-  res.send("API is running ...");
-});
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -28,6 +25,18 @@ app.get("/api/config/paypal", (req, res) =>
 
 const folder = path.resolve();
 app.use("/uploads", express.static(path.join(folder, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.listen(
   PORT,
